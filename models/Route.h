20cfc3074f52fbcbf0fa4a272d69c580e0ac92c7
iwen Mahe <116281506+iwen58@users.noreply.github.com>
2026-04-06 22:56:22 +0200

    Update Route.h

--- models/Route.h
+++ models/Route.h
@@ -1,36 +1,51 @@
-#ifndef ROUTE_H
-#define ROUTE_H
-
 #include <iostream>
-#include <string>
-#include <vector>
-#include <iomanip>
-#include <sstream>
+#include <QApplication>
+#include <QMessageBox>
 
-class Route {
-	private :
-		int i_deb;
-		int i_fin;
-		int distance;
-	public :
-		Route(char debut='', char fin='', int distance=0): i_deb(debut), i_fin(fin), distance(distance){}
-		Route(){}
-		int getIDeb() const{
-			return i_deb;
-		}
-		int getIFin() const{
-			return i_fin;
-		}
-		int getDistance() const{
-			return distance;
-		}
-		std::string getInfos()const{
-			std::ostringstream texte;
-			texte << "Route de "<< i_deb << " à " << i_fin << "(distance : " << distance << "\n";
-			return texte.str();
-		}
-		void affiche() const{
-			std::cout << "Route de "<< i_deb << " à " << i_fin << "(distance : " << distance << "\n";
-		}
-};
-#endif
+/*===Include of project===*/
+#include <cppconn/exception.h>
+#include "model/Carte.hpp"
+#include "model/BDD.hpp"
+#include "model/SceneCarte.hpp"
+#include "view/MainWindow.hpp"
+#include "view/LoginDialog.hpp"
+
+int main (int argc, char **argv){
+    QApplication app(argc,argv);
+    LoginDialog dlg;
+    std::string host, base, user, pwd;
+
+
+    if(!dlg.exec()){
+        std::cout << "Sortie de l'application\n";
+        return 1;
+    }
+
+    //Récupération des saisies après fermeture de la Dialog box
+    dlg.getResult(host, base, user, pwd);
+    std::cout << "Lecture base routeplanner" << std::endl;
+    Carte carte;
+
+    /*host = "localhost";
+    base = "routeplanner";
+    user = "routePlanner";
+    pwd = "azerty1234";*/
+
+    try {
+        BDD bdd("tcp://"+host+":3306",base,user,pwd);
+        carte = bdd.getCarte();
+        std::cout << "Successfully get the map !! \n";
+    }
+    catch (sql::SQLException &e){
+        std::cout << "Erreur MYSQL, sortie du programme\n";
+        QMessageBox msg(QMessageBox::Critical, "Erreur mySQL", e.what());
+        msg.exec();
+        return 1;
+    }
+
+
+    MainWindow mw(carte);
+    mw.show();
+
+    return app.exec();
+}
