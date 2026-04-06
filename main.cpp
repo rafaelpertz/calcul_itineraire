1b3a3823e967ae1748ffc1355185d70f06bbe464
rafaelpertz <rafael.pereira.touze@gmail.com>
2026-04-06 23:31:13 +0200

    modif main

--- main.cpp
+++ main.cpp
@@ -2,50 +2,57 @@
 #include <QApplication>
 #include <QMessageBox>
 
-/*===Include of project===*/
 #include <cppconn/exception.h>
 #include "model/Carte.hpp"
 #include "model/BDD.hpp"
-#include "model/SceneCarte.hpp"
 #include "view/MainWindow.hpp"
 #include "view/LoginDialog.hpp"
 
-int main (int argc, char **argv){
-    QApplication app(argc,argv);
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
     LoginDialog dlg;
     std::string host, base, user, pwd;
 
-
-    if(!dlg.exec()){
-        std::cout << "Sortie de l'application\n";
-        return 1;
+    if (!dlg.exec())
+    {
+        std::cout << "Application fermee par l'utilisateur." << std::endl;
+        return 0;
     }
 
-    //Récupération des saisies après fermeture de la Dialog box
     dlg.getResult(host, base, user, pwd);
-    std::cout << "Lecture base routeplanner" << std::endl;
+
     Carte carte;
 
-    /*host = "localhost";
-    base = "routeplanner";
-    user = "routePlanner";
-    pwd = "azerty1234";*/
+    try
+    {
+        std::cout << "Connexion a la base de donnees..." << std::endl;
 
-    try {
-        BDD bdd("tcp://"+host+":3306",base,user,pwd);
+        BDD bdd("tcp://" + host + ":3306", base, user, pwd);
         carte = bdd.getCarte();
-        std::cout << "Successfully get the map !! \n";
+
+        std::cout << "Chargement des donnees reussi !" << std::endl;
+
+        if (carte.getWaypoints().empty())
+        {
+            throw std::runtime_error("Aucun waypoint charge !");
+        }
     }
-    catch (sql::SQLException &e){
-        std::cout << "Erreur MYSQL, sortie du programme\n";
-        QMessageBox msg(QMessageBox::Critical, "Erreur mySQL", e.what());
-        msg.exec();
+    catch (sql::SQLException &e)
+    {
+        QMessageBox::critical(nullptr, "Erreur MySQL", e.what());
         return 1;
     }
+    catch (std::exception &e)
+    {
+        QMessageBox::critical(nullptr, "Erreur", e.what());
+        return 1;
+    }
+    
+    MainWindow mainWindow(carte);
+    mainWindow.setWindowTitle("Calcul d'itineraire");
 
-
-    MainWindow mw(carte);
-    mw.show();
-
+    mainWindow.show();
     return app.exec();
 }
