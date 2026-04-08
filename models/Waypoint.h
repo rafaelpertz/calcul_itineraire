46c5f1d5c968a39a7b2e77925d8ad1015b446fe3
rafaelpertz <rafael.pereira.touze@gmail.com>
2026-04-08 22:20:58 +0200

    Remove redundant code and fix header guard

--- models/Waypoint.h
+++ models/Waypoint.h
@@ -58,47 +58,4 @@ class Waypoint {
 		 */
 		virtual bool isVille() const { return false; }
 };
-#endif#ifndef WAYPOINT_H
-#define WAYPOINT_H
-
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <sstream>
-
-class Waypoint {
-	protected :
-		std::string nom;
-		float lon;
-		float lat;
-	public : 
-		Waypoint (std::string nom="", float lon=0, float lat=0): nom(nom), lon(lon), lat(lat) {}
-		~Waypoint(){}
-		
-		std::string getNom() const {
-			return nom;
-		}
-		
-		float getLon() const{
-			return lon;
-		}
-
-		float getLat() const{
-			return lat;
-		}
-			
-		std::string getInfos() const{
-			std::ostringstream texte;
-			texte << nom << " : lat =" << lat << "; lon =" << lon;
-			return texte.str();
-		}
-		
-		void affiche() const{
-			std::cout << nom << " Longitude: " << lon << " Latitude: " << lat << "\n";
-		}
-		
-		virtual bool isVille() const {
-			return false;
-		}
-};
 #endif
