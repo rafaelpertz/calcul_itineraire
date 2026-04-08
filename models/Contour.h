3f4d709e43e45b15cfccb7aaaa9006a0b8b823da
rafaelpertz <rafael.pereira.touze@gmail.com>
2026-04-08 22:20:08 +0200

    Add documentation comments to Contour class methods

--- models/Contour.h
+++ models/Contour.h
@@ -5,21 +5,37 @@
 #include <vector>
 #include "Point.h"
 
+/**
+ * @class Contour
+ * @brief Représente le contour géographique de la carte sous forme d'un polygone
+ *        défini par une liste ordonnée de points géographiques.
+ */
 class Contour {
 	private:
-		std::vector<Point> points;
+		std::vector<Point> points; ///< Liste ordonnée des points formant le contour
+
 	public:
+		/// @brief Constructeur par défaut.
 		Contour() {}
+
+		/**
+		 * @brief Constructeur avec une liste de points existante.
+		 * @param points Vecteur de points géographiques
+		 */
 		Contour(std::vector<Point> points) : points(points) {}
 		~Contour() {}
 
-		std::vector<Point> getPoints() const {
-		    return points;
-		}
+		/**
+		 * @brief Retourne la liste des points du contour.
+		 * @return Vecteur de points géographiques
+		 */
+		std::vector<Point> getPoints() const { return points; }
 
-		void ajoutUnPoint(const Point& pt) {
-		    points.push_back(pt);
-		}
+		/**
+		 * @brief Ajoute un point à la fin du contour.
+		 * @param pt Point géographique à ajouter
+		 */
+		void ajoutUnPoint(const Point& pt) { points.push_back(pt); }
 };
 
 #endif
