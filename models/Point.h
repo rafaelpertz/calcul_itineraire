b2f594f2e0ea6779e90c39ecc728aa537e86afad
rafaelpertz <rafael.pereira.touze@gmail.com>
2026-04-08 22:17:40 +0200

    Enhance Point class with documentation comments
    Added documentation comments to the Point class and its members.

--- models/Point.h
+++ models/Point.h
@@ -6,22 +6,41 @@
 #include <iomanip>
 #include <sstream>
 
+/**
+ * @class Point
+ * @brief Représente un point géographique identifié par un numéro, une latitude et une longitude.
+ *        Utilisé pour définir le contour géographique de la carte.
+ */
 class Point {
 	private :
-		int num;
-		float lon;
-		float lat;
+		int num;    ///< Identifiant numérique du point (numéro d'ordre dans le contour)
+		float lon;  ///< Longitude en degrés décimaux
+		float lat;  ///< Latitude en degrés décimaux
 
-	public : 
+	public :
+		/**
+		 * @brief Constructeur d'un point géographique.
+		 * @param num Numéro d'ordre du point
+		 * @param lat Latitude en degrés décimaux
+		 * @param lon Longitude en degrés décimaux
+		 */
 		Point (int num, float lat, float lon)
 			: num(num), lon(lon), lat(lat) {}
 
 		~Point (){}
 
+		/**
+		 * @brief Retourne la longitude du point.
+		 * @return Longitude en degrés décimaux
+		 */
 		float getLon() const {
 			return lon;
 		}
 
+		/**
+		 * @brief Retourne la latitude du point.
+		 * @return Latitude en degrés décimaux
+		 */
 		float getLat() const {
 			return lat;
 		}
